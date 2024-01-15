8bd256625f2e08186bb54e99499b7f47d8e66cfa
Parvez <parvezimohammed@gmail.com>
2024-01-15 23:25:23 +0530

    Added HotelService as Service Layer

---
+++ src/main/java/app/booking/Resortle/service/HotelService.java
@@ -0,0 +1,10 @@
+package app.booking.Resortle.service;
+
+import app.booking.Resortle.dto.HotelResponse;
+
+import java.util.List;
+
+public interface HotelService {
+
+    List<HotelResponse> getHotelList();
+}
