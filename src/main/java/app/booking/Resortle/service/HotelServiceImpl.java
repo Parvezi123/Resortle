2902b32b2dd9b264ea7d0d156fce1ab07156c14e
Parvez <parvezimohammed@gmail.com>
2024-01-16 22:30:11 +0530

    Added getHotelById API in Controller & Service Layer

--- src/main/java/app/booking/Resortle/service/HotelServiceImpl.java
+++ src/main/java/app/booking/Resortle/service/HotelServiceImpl.java
@@ -1,8 +1,12 @@
 package app.booking.Resortle.service;
 
+import app.booking.Resortle.dto.HotelRequest;
 import app.booking.Resortle.dto.HotelResponse;
+import app.booking.Resortle.model.Hotel;
 
 import java.util.List;
+
+import static java.util.UUID.randomUUID;
 
 public class HotelServiceImpl implements HotelService {
     @Override
@@ -19,8 +23,34 @@ public class HotelServiceImpl implements HotelService {
                 .hotelLocation("Hyderabad")
                 .hotelZipcode("65220")
                 .build();
-
-
         return List.of(hotel1, hotel2);
+    }
+
+    @Override
+    public HotelResponse getHotelById(String hotelId) {
+        return HotelResponse.builder()
+                .hotelName("Mumbai Hotels")
+                .hotelAddress("Goregoan")
+                .hotelLocation("Navi Mumbai")
+                .hotelZipcode("670003")
+                .build();
+    }
+
+    @Override
+    public HotelResponse addHotel(HotelRequest hotelRequest) {
+        Hotel hotel = Hotel.builder()
+                .hotelId(randomUUID().toString())
+                .hotelName(hotelRequest.hotelName())
+                .hotelAddress(hotelRequest.hotelAddress())
+                .hotelLocation(hotelRequest.hotelLocation())
+                .hotelZipcode(hotelRequest.hotelZipcode())
+                .build();
+        System.out.println("Generated Hotel Id: " + hotel.hotelId() + " HotelName: " + hotel.hotelName());
+        return HotelResponse.builder()
+                .hotelName(hotel.hotelName())
+                .hotelAddress(hotel.hotelAddress())
+                .hotelLocation(hotel.hotelLocation())
+                .hotelZipcode(hotel.hotelZipcode())
+                .build();
     }
 }
