ab9f7934292c46cc4a15864be50cb4e3df24a13f
Parvez Alam <39627199+Parvezi123@users.noreply.github.com>
2024-01-21 23:59:49 +0530

    Hotel repository ft (#15)
    * delete Hotel By Id API
    
    * Refactored code
    
    * Create dummy.txt added
    
    * Refactored code
    
    * Removed dummy file

--- src/main/java/app/booking/Resortle/service/RoomService.java
+++ src/main/java/app/booking/Resortle/service/RoomService.java
@@ -1,11 +1,9 @@
 package app.booking.Resortle.service;
 
-import app.booking.Resortle.dto.HotelInfo;
 import app.booking.Resortle.dto.RoomInfo;
 
 public interface RoomService {
 
-    HotelInfo retrieveHotelInfo(String hotelId);
+    RoomInfo retrieveRoomInfo(String roomId);
 
-    RoomInfo retrieveRoomInfo(String hotelId, String roomId);
 }
--- src/main/java/app/booking/Resortle/service/RoomServiceImpl.java
+++ src/main/java/app/booking/Resortle/service/RoomServiceImpl.java
@@ -1,43 +1,13 @@
 package app.booking.Resortle.service;
 
-import app.booking.Resortle.dto.HotelInfo;
 import app.booking.Resortle.dto.RoomInfo;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class RoomServiceImpl implements RoomService {
 
     @Override
-    public HotelInfo retrieveHotelInfo(String hotelId) {
-        RoomInfo roomInfo1 = RoomInfo.builder()
-                .roomNo("1A101")
-                .roomType("Executive Room")
-                .roomCapacity(2)
-                .roomPrice(2598.0)
-                .roomBookedBy("None")
-                .roomStatus("Available")
-                .build();
-        RoomInfo roomInfo2 = RoomInfo.builder()
-                .roomNo("1A102")
-                .roomType("Suite Room")
-                .roomCapacity(4)
-                .roomPrice(4578.0)
-                .roomBookedBy("None")
-                .roomStatus("Available")
-                .build();
-        return HotelInfo.builder()
-                .hotelName("Pandya Hotels")
-                .hotelAddress("24th lane, Main Rd, Angadi")
-                .hotelLocation("Madurai")
-                .hotelZipcode("562289")
-                .rooms(List.of(roomInfo1, roomInfo2))
-                .build();
-    }
-
-    @Override
-    public RoomInfo retrieveRoomInfo(String hotelId, String roomId) {
+    public RoomInfo retrieveRoomInfo(String roomId) {
         return RoomInfo.builder()
                 .roomNo("1A101")
                 .roomType("Executive Room")
