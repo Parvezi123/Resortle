d1b5554496bf9ef41e3e9562bcf85b99df4adb8c
Parvez <parvezimohammed@gmail.com>
2024-01-22 23:53:59 +0530

    Refactored Code

--- src/main/java/app/booking/Resortle/service/HotelServiceImpl.java
+++ src/main/java/app/booking/Resortle/service/HotelServiceImpl.java
@@ -11,29 +11,13 @@ import static java.util.UUID.randomUUID;
 public class HotelServiceImpl implements HotelService {
     @Override
     public List<HotelResponse> getHotelList() {
-        HotelResponse hotel1 = HotelResponse.builder()
-                .hotelName("Taj Hotel")
-                .hotelAddress("Andheri West Post")
-                .hotelLocation("Mumbai")
-                .hotelZipcode("400001")
-                .build();
-        HotelResponse hotel2 = HotelResponse.builder()
-                .hotelName("Minerva Grand")
-                .hotelAddress("SD Road, Main Street")
-                .hotelLocation("Hyderabad")
-                .hotelZipcode("65220")
-                .build();
-        return List.of(hotel1, hotel2);
+
+        return List.of(null, null);
     }
 
     @Override
     public HotelResponse getHotelById(String hotelId) {
-        return HotelResponse.builder()
-                .hotelName("Mumbai Hotels")
-                .hotelAddress("Goregoan")
-                .hotelLocation("Navi Mumbai")
-                .hotelZipcode("670003")
-                .build();
+        return null;
     }
 
     @Override
