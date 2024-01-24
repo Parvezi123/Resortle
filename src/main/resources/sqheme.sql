66d002486a2d0891dfa2744db9c875681952a98c
Parvez <parvezimohammed@gmail.com>
2024-01-24 23:27:53 +0530

    Refactored Code

--- src/main/resources/sqheme.sql
+++ src/main/resources/sqheme.sql
@@ -1,19 +1,22 @@
--- Contents of schema.sql
-CREATE TABLE hotel (
-                       hotel_id VARCHAR(255) PRIMARY KEY,
-                       hotel_name VARCHAR(255),
-                       room_id VARCHAR(255),
-                       hotel_location VARCHAR(255),
-                       hotel_address VARCHAR(255),
-                       hotel_zipcode VARCHAR(20),
-                       FOREIGN KEY (room_id) REFERENCES room(room_id)
+-- schema.sql
+
+-- Create the hotel table
+CREATE TABLE IF NOT EXISTS hotel (
+                                     hotel_id VARCHAR(255) PRIMARY KEY,
+                                     hotel_name VARCHAR(255),
+                                     room_id VARCHAR(255),
+                                     hotel_location VARCHAR(255),
+                                     hotel_address VARCHAR(255),
+                                     hotel_zipcode VARCHAR(255)
 );
-CREATE TABLE room (
-                      room_id VARCHAR(255) PRIMARY KEY,
-                      room_type VARCHAR(255),
-                      room_no VARCHAR(10),
-                      room_capacity INTEGER,
-                      room_booked_by VARCHAR(255),
-                      room_price DOUBLE PRECISION,
-                      room_status VARCHAR(50)
+
+-- Create the room table
+CREATE TABLE IF NOT EXISTS room (
+                                    room_id VARCHAR(255) PRIMARY KEY,
+                                    room_type VARCHAR(255),
+                                    room_no VARCHAR(255),
+                                    room_capacity INTEGER,
+                                    room_booked_by VARCHAR(255),
+                                    room_price DOUBLE PRECISION,
+                                    room_status VARCHAR(255)
 );
