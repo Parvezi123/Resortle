a0f0646eda3970e1eead5349fd31a24beea5af9c
Parvez <parvezimohammed@gmail.com>
2024-02-04 23:39:03 +0530

    Refactored Repo Setup

--- src/main/java/app/booking/Resortle/dto/HotelResponse.java
+++ src/main/java/app/booking/Resortle/dto/HotelResponse.java
@@ -2,10 +2,8 @@ package app.booking.Resortle.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 
 @Builder
-@NoArgsConstructor(force = true)
 @AllArgsConstructor
 public class HotelResponse {
     String hotelName;
