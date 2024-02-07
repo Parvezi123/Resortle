be34c98d22e1452e35052faeaec151b0379fa776
Parvez Alam <39627199+Parvezi123@users.noreply.github.com>
2024-02-07 22:33:23 +0530

    Hotel repository ft (#28)
    * delete Hotel By Id API
    
    * Refactored code
    
    * Create dummy.txt added
    
    * Refactored code
    
    * Removed dummy file
    
    * Refactored Code
    
    * Refactored Infra
    
    * Refactored Code
    
    * Refactored docker-compose.yml
    
    * Refactored docker-compose.yml
    
    * Refactored Repo Setup
    
    * Refactored Repo Setup
    
    * Refactored Repo Setup
    
    * Refactored Repo Setup
    
    * Refactored Repo Setup
    
    * Refactored Repo Setup
    
    * Refactored Repo Setup
    
    * Refactored Repo Setup
    
    * Refactored Repo Setup

--- src/main/resources/sqheme.sql
+++ src/main/resources/sqheme.sql
@@ -10,7 +10,6 @@ CREATE TABLE IF NOT EXISTS hotel
     hotel_address  VARCHAR(255),
     hotel_zipcode  VARCHAR(255)
 );
-
 -- Create the room table
 CREATE TABLE IF NOT EXISTS room (
                                     room_id VARCHAR(255) PRIMARY KEY,
@@ -21,7 +20,6 @@ CREATE TABLE IF NOT EXISTS room (
                                     room_price DOUBLE PRECISION,
                                     room_status VARCHAR(255)
 );
-
 -- Insert values into the room table
 INSERT INTO room (room_id, room_type, room_no, room_capacity, room_booked_by, room_price, room_status)
 VALUES
@@ -33,18 +31,15 @@ VALUES
     ('106', 'Suite', '106F', 3, 'None', 2200.00, 'Available'),
     ('107', 'Single', '107G', 1, 'None', 1100.00, 'Available'),
     ('108', 'Double', '108H', 2, 'None', 1600.00, 'Available');
-
 -- Insert values into the hotel table with corresponding room_id
 INSERT INTO hotel (hotel_id, hotel_name, room_id, hotel_location, hotel_address, hotel_zipcode)
 VALUES
     ('1', 'Hotel Paradise', '101', 'Chennai', '123 Anna St', '600078'),
     ('2', 'Hotel Richards', '105', 'Bengaluru', '456 IndiraNagar St', '560067'),
     ('3', 'Hotel SunVilla', '107', 'Hyderabad', '789 GachiBowli St', '768890');
-
 -- Insert additional values into the hotel table with corresponding room_id
 INSERT INTO hotel (hotel_id, hotel_name, room_id, hotel_location, hotel_address, hotel_zipcode)
 VALUES
     ('4', 'Hotel Danchiards', '102', 'Chennai', '101 Perambalur St', '45678'),
     ('5', 'Hotel Mahendra', '104', 'Pune', '202 Highstreet Ln', '98765'),
     ('6', 'Hotel Cityknights', '108', 'Bhopal', '303 AurovillaSantualry Rd', '54321');
-
